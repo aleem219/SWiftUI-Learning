316ba6e9257edf0c7345c3adbffc8c86987ba795
Your GitHub aleem219 <Your GitHub qazialeem21@gmail.com>
2026-01-03 15:24:40 +0530

    vc

---
+++ Learning_SWIFTUI/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Learning_SWIFTUI
+//
+//  Created by Abdul Aleem on 03/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+        // abdul
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Learning_SWIFTUI/Learning_SWIFTUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_SWIFTUIApp.swift
+//  Learning_SWIFTUI
+//
+//  Created by Abdul Aleem on 03/01/26.
+//
+
+import SwiftUI
+
+@main
+struct Learning_SWIFTUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
