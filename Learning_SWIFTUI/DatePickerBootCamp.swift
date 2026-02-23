90e9d05dc445bb851622757e965bc8c2f713d857
Your GitHub aleem219 <Your GitHub qazialeem21@gmail.com>
2026-02-23 23:14:24 +0530

    How to use DatePicker to select dates in SwiftUI | Bootcamp #40

---
+++ Learning_SWIFTUI/DatePickerBootCamp.swift
@@ -0,0 +1,47 @@
+//
+//  DatePickerBootCamp.swift
+//  Learning_SWIFTUI
+//
+//  Created by Abdul Aleem on 23/02/26.
+//
+
+import SwiftUI
+
+struct DatePickerBootCamp: View {
+    @State var selectedDate:Date = Date()
+    let startingDate:Date = Calendar.current.date(from: DateComponents(year: 2018))!
+    let endingDate:Date = Date()
+    
+    var dateFormatter: DateFormatter {
+        let formatteer = DateFormatter()
+        formatteer.dateStyle = .medium
+        return formatteer
+    }
+    
+    
+    var body: some View {
+        
+        VStack {
+            //        DatePicker("Select a Date", selection: $selectedDate)
+            //   DatePicker("Select a Date",
+            //              selection: $selectedDate,
+            //              displayedComponents: [.date])
+            Text("Selected Date is:")
+            Text(dateFormatter.string(from: selectedDate))
+                .font(.title)
+                    DatePicker("Select a Date",
+                               selection: $selectedDate,
+                               in: startingDate...endingDate, displayedComponents: [.date])
+                        .accentColor(Color.green)
+            //            .datePickerStyle(GraphicalDatePickerStyle())
+                        .datePickerStyle(CompactDatePickerStyle())
+        }
+        
+    }
+}
+
+#Preview {
+    DatePickerBootCamp()
+}
+
+
